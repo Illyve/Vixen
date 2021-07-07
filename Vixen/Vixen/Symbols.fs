8a050d799ef294fd705457fd10b0f8584d0cd1fd
Illyve <annamntx@gmail.com>
2021-07-06 20:54:22 -0500

    Implemented symbol adding

--- Vixen/Vixen/Symbols.fs
+++ Vixen/Vixen/Symbols.fs
@@ -1,10 +1,13 @@
 ﻿module Symbols
 
 type Table<'T> (entries : Map<string, 'T> list) =
-    member private this.list = entries
+    member private this.list = 
+        match entries with
+        | [] -> [ Map.empty ]
+        | _ -> entries
     member this.Add (key, value) = 
         match this.list with
-        | head :: tail when not (head.ContainsKey key) -> new Table<'T> ((head.Add (key, value)) :: this.list)
+        | head :: tail when not (head.ContainsKey key) -> new Table<'T> ((head.Add (key, value)) :: tail)
         | _ -> invalidArg key $"The key {key} already exists in the table"
     member this.Enter () =
         new Table<'T> (Map.empty :: this.list)
@@ -28,7 +31,9 @@ type StorageType =
     Dynamic | Static
 
 type Symbol<'T> =
-    | Global of {| Type : string; AccessType : AccessType; StorageType : StorageType; Value : 'T |}
-    | Local of {| Type : string; AccessType : AccessType; StorageType : StorageType; Position : int; Value : 'T |}
-    | Member of {| Type : string; AccessType : AccessType; StorageType : StorageType; Offset : int; Value : 'T |}
-    | Definition of {| AccessType : AccessType; StorageType : StorageType; Size : int; Value : 'T |}
+    | Global of Type : string * AccessType : AccessType * StorageType : StorageType * Value : 'T
+    | Function of Type : string
+    | Local of Type : string * AccessType : AccessType * StorageType : StorageType * Position : int * Value : 'T
+    | Parameter of Type : string * AccessType : AccessType * StorageType : StorageType * Position : int
+    | Member of Type : string * AccessType : AccessType * StorageType : StorageType * Offset : int * Value : 'T
+    | Definition of AccessType : AccessType * StorageType : StorageType * Size : int * Value : 'T 
